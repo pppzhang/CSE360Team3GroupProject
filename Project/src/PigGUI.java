22e0addabee5dda389e6df0fede9f42ab11bbaec
Phillip S. Reyes <psreyess@asu.edu>
2016-04-13 23:38:47 -0700

    Just added stubs to PigGui

--- Project/src/PigGUI.java
+++ Project/src/PigGUI.java
@@ -1,15 +1,74 @@
 
+/**
+ * 
+ * @author Philllip Reyes 
+ *
+ */
 public class PigGUI {
 	
 	String[] stats;
-
-	public static void main(String[] args) {
-		PigGUI application = new PigGUI();
-	}
-	
+	/**
+	 * 
+	 */
 	public PigGUI() {
 		//TODO Peng - load stats
 		//TODO start GUI
 	}
-
+	/**
+	 * @param playerIDs
+	 */
+	public void setOrder(int [] playerIDs){
+		
+	}
+	/**
+	 * 
+	 * @param oldValue
+	 */
+	public void rollOther (int oldValue){
+		
+	}
+	/**
+	 * 
+	 * @param newValue
+	 */
+	public void roll(int newValue){
+		
+	}
+	/**
+	 * 
+	 * @param name
+	 */
+	public void join(String name){
+		
+	}
+	/**
+	 * 
+	 * @param playerID
+	 */
+	public void  leave(int playerID){
+		
+	}
+	/**
+	 * 
+	 * @param playerID
+	 */
+	public void setTurn(int playerID){
+		
+	}
+	/**
+	 * 
+	 * @param lastValues
+	 */
+	public void reveal(int [] lastValues){
+		
+	}
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		PigGUI application = new PigGUI();
+	}
+	
+	
 }
