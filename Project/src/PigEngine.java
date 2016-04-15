7c85174d9e1bc3c4d23fa3ded87688811807c80c
blueridger <nath.sears@gmail.com>
2016-04-15 12:51:39 -0700

    Fixing the fact that Priyanka's stuff was deleted :P

--- Project/src/PigEngine.java
+++ Project/src/PigEngine.java
@@ -1,12 +1,54 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class PigEngine extends Thread {
 
+	private PigServer server;
+	private int numPlayers;
+	
 	public PigEngine(PigServer server, int numPlayers) {
 		// TODO Engine
+		this.server = server;
+		this.numPlayers = numPlayers;
 	}
 	
+
 	public void run() {
+		boolean end = false;
+		do
+		{
+			boolean roll = server.roll(); //TODO add arguments
+			if(roll)
+			{
+				//TODO check if playersLeft?
+			}
+			else
+			{
+				if(Rollagain)
+				{
+					continue; //roll again
+				}
+				else
+				{
+					//Bank score
+					//check if players left?
+					if(playersLeft)
+					{
+						//pass die
+						
+					}
+					else
+					{
+						//end
+						end = true;
+					}
+				}
+			}
+		}
+		while(!end);
 		
 	}
+	
 
 }
