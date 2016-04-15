fe50ff2d5dd60ec7d81a45220677683b8a4a1566
Peng Zhang <pppzhang@gmail.com>
2016-04-15 00:30:13 -0700

    Stats including username, score and roll numbers, as well as get/set method

--- Project/src/PigStats.java
+++ Project/src/PigStats.java
@@ -1,4 +1,30 @@
 
+import java.util.ArrayList;
+
 public class PigStats {
+	private String username;
+	private int score;
+	private ArrayList<Integer> rollNumber;
 	
+	public PigStats (String username) {
+		this.username = username;
+		this.score = 0;
+		this.rollNumber = new ArrayList<>();
+	}
+	
+	public void addRollNumber (int roll) {
+		rollNumber.add(roll);
+		if (roll == 1)
+			this.score = 0;
+		else
+			this.score = this.score + roll;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public ArrayList<Integer> getRollNumber() {
+		return rollNumber;
+	}
 }
