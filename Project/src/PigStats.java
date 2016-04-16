d734ff51f211d0be2b53f46704fd6e8bbf2e0602
pppzhang <pppzhang@gmail.com>
2016-04-15 23:12:45 -0700

    Add documentation and unit test to PigStats

--- Project/src/PigStats.java
+++ Project/src/PigStats.java
@@ -1,17 +1,30 @@
 
 import java.util.ArrayList;
 
+/**
+ * PigStats store player statistics and send player statistics to other classes upon request. 
+ * @author Peng Zhang
+ *
+ */
 public class PigStats {
 	private String username;
 	private int score;
 	private ArrayList<Integer> rollNumber;
 	
+	/**
+	 * Constructor needs a player's username. Initialize score and roll results.
+	 * @param username
+	 */
 	public PigStats (String username) {
 		this.username = username;
 		this.score = 0;
 		this.rollNumber = new ArrayList<>();
 	}
 	
+	/**
+	 * addRollNumber receives dice roll result and add it to score, reset score to 0 if roll number is 1.
+	 * @param roll
+	 */
 	public void addRollNumber (int roll) {
 		rollNumber.add(roll);
 		if (roll == 1)
@@ -20,10 +33,18 @@ public class PigStats {
 			this.score = this.score + roll;
 	}
 	
+	/**
+	 * getScore returns a player's score to caller
+	 * @return
+	 */
 	public int getScore() {
 		return score;
 	}
 	
+	/**
+	 * getRollNumber returns a player's every roll results to caller
+	 * @return
+	 */
 	public ArrayList<Integer> getRollNumber() {
 		return rollNumber;
 	}
